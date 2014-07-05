e105b38a5bd3f30d3422e9fd4c0b8dc28fdd6d95
Dev--xu <xushuangshuang1@gmail.com>
2014-07-05 23:16:51 +0800

    修改了编译脚本，因为清空脚本会将target下的所有目录全部删除，会导致编译失败

--- compile.sh
+++ compile.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+set -e
+
+mkdir -p target/classes
+
+mkdir -p target/test-classes
+
 javac -d target/classes  src/main/java/com/baldur/*.java
 
 javac -classpath target/classes -d target/test-classes src/test/java/com/baldur/*.java
