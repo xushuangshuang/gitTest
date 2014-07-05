43410f9273f16677f4e0c01a13a634e35196e6c7
Dev--xu <xushuangshuang1@gmail.com>
2014-07-05 23:05:37 +0800

    增加了FizzBuzz的源文件和FizzBuzzTest的测试文件

---
+++ src/main/java/com/baldur/FizzBuzz.java
@@ -0,0 +1,9 @@
+package com.baldur;
+
+public class FizzBuzz
+{
+	public String say(Integer num)
+	{
+		return String.valueOf(num);
+	}
+}
---
+++ src/test/java/com/baldur/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package com.baldur;
+
+public class FizzBuzzTest
+{
+	public static void main(String[] args)
+	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
+
+		System.out.println("1".equals(fizzBuzz.say(1)));
+	}
+}
