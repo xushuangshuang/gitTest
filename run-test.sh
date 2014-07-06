e0cb270dbab1f6e7b931568d8206e857fb2f1028
Dev--xu <xushuangshuang1@gmail.com>
2014-07-06 21:46:19 +0800

    增加了新的jar文件，这两个是在运行时必须用到的

--- run-test.sh
+++ run-test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-java -classpath "lib/reflections-0.9.9-RC2.jar;target/classes;target/test-classes" com.unit.XuShuangShuangJunit
+java -classpath "lib/reflections-0.9.9-RC2.jar;target/classes;target/test-classes;lib/guava-15.0.jar;lib/javassist-3.18.2-GA.jar" com.unit.XuShuangShuangJunit
 
 echo "run-test  OVER"
