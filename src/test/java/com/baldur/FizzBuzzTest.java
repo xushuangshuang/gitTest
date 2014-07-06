8d59fb0975f0ec91bf8775da89f555aea0a9b0d4
Dev--xu <xushuangshuang1@gmail.com>
2014-07-06 09:29:14 +0800

    增加了案例2和3

--- src/test/java/com/baldur/FizzBuzzTest.java
+++ src/test/java/com/baldur/FizzBuzzTest.java
@@ -7,5 +7,7 @@ public class FizzBuzzTest
 		FizzBuzz fizzBuzz = new FizzBuzz();
 
 		System.out.println("1".equals(fizzBuzz.say(1)));
+		System.out.println("2".equals(fizzBuzz.say(2)));
+		System.out.println("Fizz".equals(fizzBuzz.say(3)));
 	}
 }
