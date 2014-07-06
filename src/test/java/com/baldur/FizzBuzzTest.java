ae1398a0928b38c19d402c85b73189183ce5b3bc
Dev--xu <xushuangshuang1@gmail.com>
2014-07-06 10:13:38 +0800

    增加对测试的计数功能，并且可以打印出详细的测试信息

--- src/test/java/com/baldur/FizzBuzzTest.java
+++ src/test/java/com/baldur/FizzBuzzTest.java
@@ -2,25 +2,52 @@ package com.baldur;
 
 public class FizzBuzzTest
 {
+  	static boolean testResult = true;
+	static int testSuccess = 0;
+	static int testFail = 0;
 	public static void main(String[] args)
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
 		String actualResult = fizzBuzz.say(1);
-		if("1".equals(actualResult))
+		if(!"1".equals(actualResult))
 		{
+		    	testFail++;
 			System.out.println("EXPECTED " + "1" + "  but  " + actualResult);
+		}
+		else
+		{
+			testSuccess++;
 		}
 
 		actualResult = fizzBuzz.say(2);
-		if("2".equals(actualResult))
+		if(!"2".equals(actualResult))
 		{
+		    	testFail++;
 			System.out.println("EXPECTED " + "2" + "  but  " + actualResult);
+		}
+		else
+		{
+			testSuccess++;
 		}
 
 		actualResult = fizzBuzz.say(3);
-		if("Fizz".equals(actualResult))
+		if(!"Fizz".equals(actualResult))
 		{
+		    	testFail++;
 			System.out.println("EXPECTED " + "Fizz" + "  but  " + actualResult);
+		}
+		else
+		{
+			testSuccess++;
+		}
+
+		if(testResult)
+		{
+			System.out.println("TESTSUCCESS :" + testSuccess + "  TESTFAIL :" + "0");
+		}
+		else
+		{
+			System.out.println("TESTFALI :" + testFail );
 		}
 
 	}
