3c46640a4f8384673971b2a7f451f5cefde37f4e
Dev--xu <xushuangshuang1@gmail.com>
2014-07-06 17:22:30 +0800

    因为刚才把Junit放到了unit目录下。所以修改了脚本，并且编译完成后打印出编译成功

--- compile.sh
+++ compile.sh
@@ -8,4 +8,6 @@ mkdir -p target/test-classes
 
 javac -d target/classes  src/main/java/com/baldur/*.java
 
-javac -classpath target/classes -d target/test-classes src/test/java/com/baldur/*.java
+javac -classpath target/classes -d target/test-classes  src/test/java/com/unit/*.java src/test/java/com/baldur/*.java
+
+echo "compile.sh  SUCCESS"
