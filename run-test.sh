4b3218c7a4a72caf73d871fcceb7b28be15a3aa8
Dev--xu <xushuangshuang1@gmail.com>
2014-07-06 18:24:51 +0800

    增加了一个判断，如果脚本中有一句命令执行错误就不在执行下面的命令

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-java -classpath "target/classes;target/test-classes" com.unit.XuShuangShuangJunit
+set -e
+
+java -classpath "lib/reflections-0.9.9-RC2.jar;target/classes;target/test-classes" com.unit.XuShuangShuangJunit
 
 echo "run-test  OVER"
