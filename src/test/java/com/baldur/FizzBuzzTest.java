633832ac21a25102aaee782a630f0df2b99d7de4
Dev--xu <xushuangshuang1@gmail.com>
2014-07-06 16:17:29 +0800

    修改了案例方法的调用

--- src/test/java/com/baldur/FizzBuzzTest.java
+++ src/test/java/com/baldur/FizzBuzzTest.java
@@ -6,22 +6,22 @@ public class FizzBuzzTest extends XuShuangShuangJunit
 	public void test_Fizz_should_be_3()
 	{
 		String actualResult = fizzBuzz.say(3);
-		assertEquals("3", !"Fizz".equals(actualResult));
+		assertEquals("Fizz", actualResult);
 	}
 	public void test_2_should_be_2()
 	{
 		String actualResult = fizzBuzz.say(2);
-		assertEquals("2", !"2".equals(actualResult));
+		assertEquals("2", actualResult);
 	}
 	public void test_1_should_be_1()
 	{
 		String actualResult = fizzBuzz.say(1);
-		assertEquals("1", !"1".equals(actualResult));
+		assertEquals("1", actualResult);
 	}
 	public void test_Buzz_should_be_5()
 	{
 		String actualResult = fizzBuzz.say(5);
-		assertEquals("Buzz", !"Buzz".equals(actualResult));
+		assertEquals("Buzz", actualResult);
 	}
 }
 
